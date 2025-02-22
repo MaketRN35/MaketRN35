c2e32958e39a6196a4d72f3afc5eb87a73e8fb7e
Anthony <a.penhard@gmail.com>
2025-02-22 23:15:05 +0100

    V2_1_0

---
+++ Changelog.md
@@ -0,0 +1,111 @@
+# Changelog
+
+Programme développé par Anthony PENHARD pour piloter des maquettes en classe de technologie Collège Ac-Rennes
+avec carte BBC Micro:Bit qui communiques en "sans-fil" et permet d'avoir une supervisions de chaques maquettes via une interface web locale
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/)
+
+
+## [Unreleased]
+
+
+## [2.1.0] - 2025-02-22
+
+### Added
+- Ajout template spécifique pour "8 maquettes Potager connecté"" "/potager" (Demande de Dave E.)
+- Ajout parking trottinette v2 "/trott2" savoir quelles sont les places libres (Demande de Dave E.)
+- gérer de façon générique une page template perso.html si existent à coté du script et dans le config.ini
+   alors le lancer par défaut et via serveur web si localhost:5000/perso ouvrir la page perso.html si existe au lieu de "defaut.html"
+- standardisation sections config.ini pour un template personalisé :
+[defaut]
+// format des données de type : <Data_format>X:Y  avec X dans <Liste_Ilot> et Y valeur fonctionnelle pour la maquette
+Titre : Maquette de Machin truc
+Sous_titre : maquette qui mesure un machin
+// format des données de type : <Data_format>X:Y  avec X dans <Liste_Ilot> et Y valeur fonctionnelle pour la maquette
+Data_format : H
+// Y : Valeur par défaut reçue pour les maquettes
+Valeur_defaut : 100
+// liste des "noms" de chaque maquettes possible cartères chiffres ou lettres ou mélangé
+Liste_Maquettes : 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+// Nombre de maquettes à afficher sur page web 
+Nb_Maquettes : 16
+// Page web a afficher ( par défaut nom de la maquette <name>.hmtl )
+Page_html : defaut.html
+// Page web texte pour chaque Maquette
+Texte_Maquette : Maquette
+// image a afficher pour chauqe maquette dans la Page web <name>XX.png 
+// (defaut.png, carte.png, parking.jpg, bipeur.png, potager.png , potager(1 à 8).png, barriere.png, trott2_(0 à 7).png)
+Img_Maquette : potager1.png, potager2.png, potager3.png, potager4.png, potager5.png, potager6.png, potager7.png, potager8.png
+// image a afficher si liste true OU false affiche la même image pour chacun et donc Img_Maquette n'a qu'1 image
+Img_Maquette_liste : true
+// affiche la carte qui affiche la dernière data reçue par défaut true
+Carte_AllData : true
+// affiche la carte pour modifier le Groupe Radio utilisé par défaut false
+Carte_MAJ_grouperadio : true
+// Paramètres "autres" envoyés au serveur web 
+Param1 : 1
+Param2 : 2
+Param3 : 3
+- Images 'static' présentes :
+favicon.png
+defaut.png
+carte.png
+parking.jpg
+potager.png
+potager(1à8).png
+trott2_(0à7).png
+bipeur.png
+barriere.png
+karotz.jpg
+- Templates intégrés :
+config
+index (page par défaut racine du serveur web)
+defaut
+lapin ( perso et tests pour Anthony P.)
+potager ( potager connecté David E.)
+trott2 ( parking trottinette v2 David E.)
+
+
+### Removed
+- Les pages/templates spécifiques (Rfid, Parking trotinettes "trott" ) => standardisation de tous ces templates spécifiques
+- Configuration spécifique des maquettes parking trotinettes
+
+### Changed
+- fichier config.ini : la section "settings" est la configuration générale du script
+[settings]  
+serial_port : COM4 // Windows (ex: "COM5")
+cnx_auto : on //connexion automatique du port série On True (par défaut) /False Off 
+baud_rate : 115200 // vistesse de connexion série
+groupe_radio : 99 // groupe radio utilisé par les cartes Micro:Bit
+
+- si fichier config pas présent et/ou partiel il y a des valeurs par défaut prises pour chaque paramètres
+- suppresion de la config spécifique au parking de trotinette pour standardisation
+
+
+## [2.0.3] - 2025-02-18
+
+### Added
+ réfelxion sur le nom et dépot sur un github : MaketRN35 ( https://github.com/MaketRN35/MaketRN35 )
+V2_0_2 : 05/02/25 : ajout a config.ini les données pour le parking trottinette
+V2_0_1 : 04/02/25 : si fichier config.ini absent, défini par défaut une config
+V2_0_0 : 02/02/25 : ajout fichier du websocket au lieu d'un pooling toutes les 1s, j'ai gardé la section api pour envoyer à la MB
+
+V1_0_1 à 1_0_3 : 23/01/25 : 
+	- ajout page Config et amélioration de index avec 20 cartes affichées
+	- ajout page RFID avec 09 ilôts
+	- ajout fichier de config.ini + pas mal de templates ...
+V1_0_0 : 22/01/25 : initiale prend le dernier pour COM pour afficher valeures reçu et possible envoi manuellement datas
+
+
+
+
+### Added
+ pour les nouvelles fonctionnalités. 
+### Changed
+ pour les changements aux fonctionnalités préexistantes. 
+### Deprecated
+ pour les fonctionnalités qui seront bientôt supprimées
+### Removed
+ pour les fonctionnalités désormais supprimées. 
+### Fixed
+ pour les corrections de bugs. 
--- microbit_server.py
+++ microbit_server.py
@@ -1,28 +1,20 @@
-#  Programme développer par Anthony PENHARD pour piloter des maquettes en classe de technologie Collège Ac-Rennes
+﻿#  Programme développer par Anthony PENHARD pour piloter des maquettes en classe de technologie Collège Ac-Rennes
 #
-# V1_0_0 : initiale prend le dernier pour COM pour afficher valeures reçu et possible envoi manuellement datas
-# V1_0_1 : ajout page Config et amélioration de index avec 20 cartes affichées
-# V1_0_2 : ajout page RFID avec 09 ilôts
-# V1_0_3 : ajout fichier de config.ini + pas mal de templates ...
-# V2_0_0 : ajout fichier du websocket au lieu d'un pooling toutes les 1s, j'ai gardé la section api pour envoyer à la MB
-# V2_0_1 : si fichier config.ini absent, défini par défaut une config
-# V2_0_2 : ajout a config.ini les données pour le parking trottinette
-# V2_0_3 : réfelxion sur le nom et dépot sur un github : Ma
+# Cf. Changelog.md
 
 import serial
 import serial.tools.list_ports
 import time
-#import csv
+import os
+import sys
 from datetime import datetime
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, render_template_string, request, send_from_directory, abort
 from configparser import ConfigParser
 from flask_socketio import SocketIO
 from engineio.async_drivers import threading
 import threading
-#import numpy as np
 
 # Variable globale pour stocker les dernières données reçues
-#latest_data = {"valeur": None, "num_carte": None}
 latest_data = {"valeur": None}
 
 # Initialisation de Flask et Flask-SocketIO
@@ -30,45 +22,55 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 # fichier de configuration ini
-# https://docs.python.org/fr/3/library/configparser.html
-
 config = ConfigParser() 
 try:
-    with open('config.ini') as f:
-        config.read('config.ini')
+    with open('config.ini', 'r', encoding='utf-8') as f:
+        config.read('config.ini', 'UTF-8')
         config_settings = config['settings']
-        if not config['Parking']:
-            config['Parking'] = {'Data_format': 'Parking', 'Nb_Parking': '16', 'Liste_Parking': 'letter'}
+        # Configuration du port série
+        SERIAL_PORT = config_settings.get('serial_port', 'COM4')
+        BAUD_RATE = int(config_settings.get('baud_rate', '115200'))
+
+        # groupe_radio utilisé par la carte micro:Bit
+        groupe_radio = int(config_settings.get('groupe_radio', '99'))
+
+        # connexion automatique On/Off
+        cnx_auto = config_settings.getboolean('cnx_auto', True)
+        mode_namevalue = config_settings.get('mode_namevalue', 0)
 except IOError:
-    print("le fichier config.ini n'est pas présent.")
-    config_settings = {'serial_port': 'COM4', 'cnx_auto': 'On', 'baud_rate': '115200', 'groupe_radio': '99'}
-    config['settings'] = config_settings
-    config['Parking'] = {'Data_format': 'Parking', 'Nb_Parking': '16', 'Liste_Parking': 'letter'}
-
-#print(vars(config))
-
-# Configuration du port série
-SERIAL_PORT = config_settings.get('serial_port', 'COM4')
-BAUD_RATE = int(config_settings.get('baud_rate', '115200'))
-
-# groupe_radio utilisé par la carte micro:Bit
-groupe_radio = int(config_settings.get('groupe_radio', '99'))
-
-# connexion automatique On/Off
-cnx_auto = bool(config_settings.get('cnx_auto', True))
+    print("Fichier config.ini non présent.")
+    # Configuration du port série
+    SERIAL_PORT = 'COM4'
+    BAUD_RATE = 115200
+    groupe_radio = 99
+    cnx_auto = True
+    mode_namevalue = 0
 
 #/fin fichier de configuration ini
 
+# Obtenir le chemin du répertoire où l'exécutable est lancé
+if getattr(sys, 'frozen', False):
+    # Le script est exécuté en tant qu'exécutable
+    root_dir = sys._MEIPASS
+else:
+    # Le script est exécuté normalement
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+
 # affiche la liste des ports disponibles
+print("Ports COM trouvés : ( on prend le dernier )")
 serial_ports = serial.tools.list_ports.comports()
 for port in serial_ports:
     SERIAL_PORT = port.device
     print(f"{port.name} // {port.device} // D={port.description}")
-print(len(serial_ports), ' ports COM trouvés on prend le dernier')
 
 time.sleep(1)
-ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+try:
+    ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+    print("Connexion au port série réussie ")
+except Exception as e:
+    print(f"Erreur connexion port série : {e}")
 time.sleep(1)
+
 # Fonction pour envoyer une commande au Micro:bit
 def send_to_microbit(value):
     global ser
@@ -95,25 +97,27 @@ def read_serial():
 t = threading.Thread(target=read_serial, daemon=True)
 def thread_serie():
     global t
+    global groupe_radio
     # Démarrer la lecture série dans un thread séparé
     t.etat = True
     t.start()
+    # dès le démarrage envoi le groupe_radio qui a pu être modifié dans le fichier config
+    send_to_microbit('groupe_radio:' + str(groupe_radio))
+    #TODO a tester send_to_microbit('mode_namevalue:' + int(mode_namevalue))
 
 # si cnx_auto est a On/True dans le fichier de config.ini alors on lance le thread tout de suite
 if cnx_auto :
     t.etat = True
     t.start()
+    # dès le démarrage envoi le groupe_radio qui a pu être modifier dans le fichier config
+    send_to_microbit('groupe_radio:' + str(groupe_radio))
+    #TODO a tester send_to_microbit('mode_namevalue:' + int(mode_namevalue))
 
 # WebSocket pour recevoir les commandes depuis la page Web
 @socketio.on('send_command')
 def handle_command(command):
     print(f"Commande reçue depuis WebSocket : {command}")
     send_to_microbit(command)
-
-# WebSocket
-@app.route('/socket')
-def socket():
-    return render_template("socket.html")
 
 # Route API pour obtenir la dernière valeur
 @app.route('/api/data', methods=['GET'])
@@ -130,38 +134,6 @@ def send_command():
         return jsonify({"status": "Envoyé", "value": value})
     return jsonify({"status": "Erreur", "message": "Valeur non valide"}), 400
 
-# Route pour servir l'interface Web
-@app.route('/index')
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/config')
-def config_interface():
-    return render_template("config.html", serial_ports=serial_ports , nb_port_COM=len(serial_ports) , BAUD_RATE=BAUD_RATE , cnx_auto=cnx_auto , groupe_radio=groupe_radio, config=config )
-
-# Route API config générale "settings"
-@app.route('/api/config', methods=['POST'])
-def send_config():
-    SERIAL_PORT = request.json.get("serial_port")
-    BAUD_RATE = request.json.get("BAUD_RATE")
-    cnx_auto = request.json.get("cnx_auto")
-    groupe_radio = request.json.get("groupe_radio")
-    print(f"Donnée config settings : {SERIAL_PORT} // {BAUD_RATE} // {cnx_auto} // {groupe_radio}")
-    return jsonify({"status": "Envoyé", "serial_port": SERIAL_PORT , "BAUD_RATE": BAUD_RATE , "cnx_auto": cnx_auto , "groupe_radio": groupe_radio })
-
-# Route API config "Parking"
-@app.route('/api/configParking', methods=['POST'])
-def send_configParking():
-    #config['Parking'] = {'Data_format': 'Parking', 'Nb_Parking': '16', 'Liste_Parking': 'letter'}
-    config['Parking']['Data_format'] = request.json.get("Data_format")
-    config['Parking']['Liste_Parking'] = request.json.get("Liste_Parking")
-    config['Parking']['Nb_Parking'] = request.json.get("Nb_Parking")
-    #print(f"Donnée config Parking : {Data_format} // {Liste_Parking} // {Nb_Parking} ")
-    print(f"Donnée config Parking : {config['Parking']['Data_format']} // {config['Parking']['Liste_Parking']} // {config['Parking']['Nb_Parking']} ")
-    #print(vars(config['Parking']))
-    return jsonify({"status": "Envoyé", "Data_format": config['Parking']['Data_format'] , "Liste_Parking": config['Parking']['Liste_Parking'] , "Nb_Parking": config['Parking']['Nb_Parking'] })
-
 # Route API start le thread de la connexion série de la carte Micro:Bit
 @app.route('/api/thread')
 def start_thread():
@@ -173,52 +145,152 @@ def stop_thread():
     global t
     # Stoppe le thread de lecture série 
     t.etat = False
+    # TODO a modifier car ne fonctionne pas ...
     t.stop()
     return "Thread Cnx Série Micro:Bit STOP"
 
+# Route pour servir l'interface Web
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template("index.html", config=config )
+
+@app.route('/config')
+def config_interface():
+    return render_template("config.html", serial_ports=serial_ports , nb_port_COM=len(serial_ports) , 
+        BAUD_RATE=BAUD_RATE , cnx_auto=cnx_auto , mode_namevalue=mode_namevalue, groupe_radio=groupe_radio, config=config )
+
+# Route API config générale "settings"
+@app.route('/api/config', methods=['POST'])
+def send_config():
+    global ser
+    SERIAL_PORT = request.json.get("serial_port")
+    BAUD_RATE = request.json.get("BAUD_RATE")
+    cnx_auto = request.json.get("cnx_auto")
+    mode_namevalue = request.json.get("mode_namevalue")
+    groupe_radio = request.json.get("groupe_radio")
+    print(f"Donnée config settings : {SERIAL_PORT} // {BAUD_RATE} // {cnx_auto} // {mode_namevalue} // {groupe_radio}")
+    time.sleep(1)
+    try:
+        ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+        print("Connexion au port série réussie ")
+    except Exception as e:
+        print(f"Erreur connexion port série : {e}")
+    time.sleep(1)
+    return jsonify({"status": "Envoyé", "serial_port": SERIAL_PORT , "BAUD_RATE": BAUD_RATE , "cnx_auto": cnx_auto , "mode_namevalue":mode_namevalue , "groupe_radio": groupe_radio })
+
+@app.route('/about')
 @app.route('/version')
 def version():
-    return "Fait par Anthony PENHARD Prof Technologie Ac-Rennes V2.0.3"
+    return "Fait par Anthony PENHARD Prof Technologie Ac-Rennes V2.1.0"
 
-# Route pour servir l'interface Web rfid
-@app.route('/rfid')
-def rfid():
-    return render_template("rfid.html")
+# utiliser un template externe qui est à la racine de l'executable et non intégré <fichier>.html
+template_externe = False
+@app.route('/external/<fichier>')
+def external_template(fichier):
+    global template_externe
+    print(f"Appel de la fonction external !! {template_externe}")
+    template_externe = True
+    return default(fichier)
 
-# Route pour servir l'interface Web lapin Karotz / Nabaztag
-@app.route('/lapin')
-def lapin():
-    return render_template("lapin.html", groupe_radio = groupe_radio)
+def template_exists(template_name):
+    # Construire le chemin complet vers le fichier template
+    template_path = os.path.join(root_dir, 'templates', template_name)
+    # Vérifier si le fichier existe
+    return os.path.exists(template_path)
 
-# Route pour servir l'interface Web Parkings Trotinettes
-@app.route('/trott')
-def trott():
-    Data_format = str(config['Parking'].get('Data_format', 'Parking'))
-    Nb_Parking = int(config['Parking'].get('Nb_Parking', '16'))
-    if config['Parking'].get('Liste_Parking') == 'number' :
-        Liste_Parking = ['01', '02', '03', '04', '05', '06', '07', '08', '09', 10, 11, 12, 13, 14, 15, 16]
+# "TODO à tester" pour pouvoir utiliser des images externes :
+# utiliser des images qui sont externes à l'executable et positionnées à la racine de l'executable
+# il est possible de gérer le path directement ici plutôt que dans les templates :
+# static_file_url = url_for('static', filename='internal_file.png')
+# external_file_url = url_for('serve_external', filename='external_image.png')
+@app.route('/imgexternal/<path:filename>')
+def serve_external(filename):
+    # Servez les fichiers externes depuis le répertoire racine
+    return send_from_directory(root_dir, filename)
+    # dans le template utiliser : 'serve_external' au lieu de 'static' et donc interne à l'executable
+    # <img src="{{ url_for('serve_external', filename=external_image) }}" alt="External Image">
+
+# /fin "TODO à tester"
+
+# Route pour servir l'interface Web Potager Connecté
+@app.route("/<name>")
+def default(name):
+    global template_externe
+    print(f"Appel template {name} ") #  {template_externe}
+    if template_externe:
+        print(f" EXTERNE via http://localhost:5000/external/{name}")
     else:
-        Liste_Parking = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-    #elif config['Parking'].get('Liste_Parking') == 'letter' :
-    #    Liste_Parking = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-    #else:  # ici config perso / compliqué a gérer a mon sens mélange de lettre et nombre parait peu logique !!
-    #    Liste_Parking = config['Parking'].get('Liste_Parking')
-    return render_template("trott.html", Data_format = Data_format, Nb_Parking = Nb_Parking, Liste_Parking = Liste_Parking, Conf_liste = config['Parking'].get('Liste_Parking'))
+        print(f" INTERNE via http://localhost:5000/{name}")
+    if name in config:
+        # si existe section [name] dans config.ini on affiche tous les key : value
+        print(f" Config : {config[name]}")
+        #print(vars(config[name]))
+        for key, value in config.items(name):
+            print(f"{key} : {value}")
 
-# Route pour servir l'interface Web Afichage data de 20 cartes 
-@app.route('/cartes')
-def cartes():
-    return render_template("cartes.html", groupe_radio = groupe_radio)
+    # la section [name] n'existe pas dans le fichier config 
+    if name not in config:
+        print(f" pas de section [{name}] dans le fichier config.ini ")
+        config[name] = {}
+    Titre = str(config[name].get('titre', 'Maquettes pour ' + name ))
+    Sous_titre  = str(config[name].get('sous_titre', ''))
+    Data_format = str(config[name].get('data_format', name))
+    Valeur_defaut = config[name].get('valeur_defaut', '0123456789')
+    # si pas de 'Liste_Maquettes' dans la section du config.ini
+    if 'liste_maquettes' not in config[name]:
+        config[name]['liste_maquettes'] = '01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20'
+    Liste_Maquettes = [e.strip() for e in config[name].get('liste_maquettes').split(',')]
+    Nb_Maquettes = int(config[name].get('nb_maquettes', '16'))
+    Img_Maquette_liste = config[name].getboolean('img_maquette_liste', False)
+    if Img_Maquette_liste :
+        Img_Maquette = [e.strip() for e in config[name].get('img_maquette').split(',')]
+    else:
+        # TODO faire un test si l'image name.png existe alors on la prend ...
+        #Img_Maquette = str(config[name].get('img_maquette', name + ".png")) 
+        Img_Maquette = str(config[name].get('img_maquette', "defaut.png"))
 
-# Route pour servir l'interface Web charts génère un graphique
-@app.route('/charts')
-def charts():
-    return render_template("charts.html")
-
-# Route pour servir l'interface Web charts génère un graphique
-@app.route('/charts_multi')
-def charts_multi():
-    return render_template("charts_multi.html")
+    Texte_Maquette = str(config[name].get('texte_maquette', 'Maquette'))
+    Carte_AllData = config[name].getboolean('carte_alldata', True)
+    Carte_MAJ_grouperadio = config[name].getboolean('carte_maj_grouperadio', False)
+    Param1 = config[name].get('param1')
+    Param2 = config[name].get('param2')
+    Param3 = config[name].get('param3')
+    # Si on viens depuis http://localhost:5000/external/{name} OU si à la racine exite name.html on prend ce fichier template
+    #if template_externe or os.path.exists(name + '.html'):
+    if os.path.exists(name + '.html'):
+        # si bien template externe et le fichier name.html existe bien à la racine de l'executable
+        template_externe = False
+        # Charge le fichier template externe
+        with open(name + '.html', 'r', encoding='utf-8') as file:
+            template_content = file.read()
+        return render_template_string(template_content, groupe_radio = groupe_radio, 
+            Titre = Titre, Sous_titre = Sous_titre, Carte_AllData = Carte_AllData,
+            Carte_MAJ_grouperadio = Carte_MAJ_grouperadio,
+            Img_Maquette = Img_Maquette, Img_Maquette_liste = Img_Maquette_liste,
+            Data_format = Data_format, Valeur_defaut = Valeur_defaut, Nb_Maquettes = Nb_Maquettes,
+            Texte_Maquette = Texte_Maquette, Liste_Maquettes = Liste_Maquettes,
+            Param1 = Param1, Param2 = Param2, Param3 = Param3)
+    else:
+        # si on arrive la et que le template_externe = True => soucis le fichier template name.html n'existe pas !
+        #if template_externe :
+        #    template_externe = False
+        #    abort(404)
+        # Charge un template interne
+        template_externe = False
+        # recupère la page web issu de la config sinon defaut.html
+        Page_web = config[name].get('Page_html', "defaut.html")
+        # on test si le template name.html existe si oui alors on l'utilise (ex lapin) à la place de defaut.html
+        if template_exists(str(name + '.html')):
+            Page_web = str(name + '.html')
+        print(f" nom page ouverte {Page_web}")
+        return render_template(Page_web, groupe_radio = groupe_radio, 
+            Titre = Titre, Sous_titre = Sous_titre, Carte_AllData = Carte_AllData,
+            Carte_MAJ_grouperadio = Carte_MAJ_grouperadio,
+            Img_Maquette = Img_Maquette, Img_Maquette_liste = Img_Maquette_liste,
+            Data_format = Data_format, Valeur_defaut = Valeur_defaut, Nb_Maquettes = Nb_Maquettes,
+            Texte_Maquette = Texte_Maquette, Liste_Maquettes = Liste_Maquettes,
+            Param1 = Param1, Param2 = Param2, Param3 = Param3)
 
 # Lancement du serveur Flask
 if __name__ == '__main__':
